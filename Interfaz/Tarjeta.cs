a35162a9934c7901bc47ee55f2474e8357938c36
Faundez21 <cfaundezs@it.ucsc.cl>
2024-08-26 22:20:16 -0400

    Parte Final

---
+++ Interfaz/Tarjeta.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Avance.Interfaz
+{
+    public class Tarjeta
+    {
+        public int NumeroTarjeta { set;get; }   
+        public int CCV { set;get; } 
+        public DateTime FechaVencimiento { set;get; }
+
+    }
+}
